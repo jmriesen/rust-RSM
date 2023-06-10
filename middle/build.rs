3ff66509bc2f5970c36f881e2f5bf112bbaac897
jmriesen <jmriesen@iastate.edu>
2023-06-10 12:03:27 -0500

    Fixing linking issue between c and rust

--- middle/build.rs
+++ middle/build.rs
@@ -5,24 +5,32 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=C/init/");
-    println!("cargo:rerun-if-changed=C/include/");
     println!("cargo:rerun-if-changed=C/compile/");
+    println!("cargo:rerun-if-changed=C/database/");
+    println!("cargo:rerun-if-changed=C/include/");
+    println!("cargo:rerun-if-changed=C/init/");
     println!("cargo:rerun-if-changed=C/runtime/");
     println!("cargo:rerun-if-changed=C/seqio/");
+    println!("cargo:rerun-if-changed=C/symbol/");
     println!("cargo:rerun-if-changed=C/util/");
+    println!("cargo:rerun-if-changed=C/xcall/");
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate bindings for.
         .header("sys/types.h")
+        //.header("C/include/*")
         .header("C/include/rsm.h")
-        //.header("C/include/util.h")
-        .header("C/include/init.h")
+        .header("C/include/compile.h")
         .header("C/include/database.h")
+        .header("C/include/error.h")
+        .header("C/include/init.h")
+        .header("C/include/opcode.h")
         .header("C/include/proto.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
+        .header("C/include/seqio.h")
+        .header("C/include/symbol.h")
+    // Tell cargo to invalidate the built crate whenever any of the
+    // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
+    // Finish the builder and generate the bindings.
         .generate()
         .expect("Unable to generate bindings");
 
@@ -36,7 +44,13 @@ fn main() {
     use std::process::Command;
     println!("{:?}",Command::new("make")
              .current_dir("./C")
-             //.arg("Hello world")
+             .arg("lib")
              .output()
              .expect("Failed to execute command"));
+    println!("cargo:rustc-link-search=native=./middle/C");
+    println!("cargo:rustc-link-lib=mylib");
+    println!("cargo:rustc-link-lib=framework=CoreServices");
+    println!("cargo:rustc-link-lib=framework=DirectoryService");
+    println!("cargo:rustc-link-lib=framework=Security");
+
 }
