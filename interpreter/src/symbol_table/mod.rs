2f1625c6453d3f6352b0ce094a18da95cb5b4174
jmriesen <jacobriesen@gmail.com>
2024-07-06 13:50:37 -0500

    Adding more pin down tests for get/set

--- interpreter/src/symbol_table/mod.rs
+++ interpreter/src/symbol_table/mod.rs
@@ -27,6 +27,7 @@ type Tab = c_code::symtab_struct;
 pub mod tests {
 
     use ffi::CSTRING;
+    use pretty_assertions::assert_eq;
 
     use crate::{
         key::{CArrayString, KeyList},
@@ -81,6 +82,13 @@ pub mod tests {
     }
 
     #[test]
+    fn get_unset_variable() {
+        let mut table = Table::new();
+        let mut m_var = var_m("foo", &[]);
+        assert_eq!(Err(-6), table.c_get(&mut m_var));
+    }
+
+    #[test]
     fn set_root_value() {
         let mut table = Table::new();
         let mut m_var = var_m("foo", &[]);
@@ -91,18 +99,82 @@ pub mod tests {
     }
 
     #[test]
-    fn get_unset_variable() {
-        let mut table = Table::new();
-        let mut m_var = var_m("foo", &[]);
-        assert_eq!(Err(-6), table.c_get(&mut m_var));
-    }
-
-    #[test]
     fn set_index_value() {
         let mut table = Table::new();
         let mut m_var = var_m("foo", &["keys"]);
         let mut data: CArrayString = "Data".into();
         table.c_set(&mut m_var, &mut data);
         assert_eq!(Ok(data), table.c_get(&mut m_var));
+    }
+
+    #[test]
+    fn set_root_then_index() {
+        let mut root = var_m("foo", &[]);
+        let mut root_data: CArrayString = "root Data".into();
+        let mut with_key = var_m("foo", &["keys"]);
+        let mut key_data: CArrayString = "key Data".into();
+        {
+            let mut table = Table::new();
+
+            table.c_set(&mut root, &mut root_data);
+            table.c_set(&mut with_key, &mut key_data);
+            assert_eq!(Ok(root_data.clone()), table.c_get(&mut root));
+            assert_eq!(Ok(key_data.clone()), table.c_get(&mut with_key));
+        }
+        {
+            let mut table = Table::new();
+
+            table.c_set(&mut with_key, &mut key_data);
+            table.c_set(&mut root, &mut root_data);
+            assert_eq!(Ok(root_data.clone()), table.c_get(&mut root));
+            assert_eq!(Ok(key_data.clone()), table.c_get(&mut with_key));
+        }
+    }
+
+    #[test]
+    fn set_null_value() {
+        let mut table = Table::new();
+        let mut m_var = var_m("foo", &[]);
+        let mut data: CArrayString = "".into();
+
+        table.c_set(&mut m_var, &mut data);
+        assert_eq!(Ok(data), table.c_get(&mut m_var));
+    }
+
+    #[test]
+    fn set_overrides_value() {
+        let mut table = Table::new();
+        let mut m_var = var_m("foo", &[]);
+        let mut initial_value: CArrayString = "inital".into();
+        let mut end_value: CArrayString = "end".into();
+
+        table.c_set(&mut m_var, &mut initial_value);
+        assert_eq!(Ok(initial_value), table.c_get(&mut m_var));
+
+        table.c_set(&mut m_var, &mut end_value);
+        assert_eq!(Ok(end_value), table.c_get(&mut m_var));
+    }
+    #[test]
+    fn do_a_bunch_of_sets() {
+        let test_data = [
+            (vec![], ""),
+            (vec!["SomeKey0"], "someKey0"),
+            (vec!["SomeKey1"], "someKey1"),
+            (vec!["SomeKey2"], "someKey2"),
+            (vec!["SomeKey3"], "someKey3"),
+            (vec!["SomeKey4"], "someKey4"),
+            (vec!["lots", "of", "Keys", "even", "more"], "lots of keys"),
+        ];
+        let mut test_data =
+            test_data.map(|(keys, value)| (var_m("foo", &keys), Box::new(value.into())));
+
+        let mut table = Table::new();
+        for (var, value) in test_data.iter_mut() {
+            table.c_set(var, value);
+        }
+
+        for (mut var, value) in test_data.into_iter() {
+            assert_eq!(Ok(*value), table.c_get(&mut var));
+        }
     }
 }
