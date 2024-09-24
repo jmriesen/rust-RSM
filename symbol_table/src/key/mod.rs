9c614b02494646b29a00bec71805b3315da13e86
jmriesen <jacobriesen@gmail.com>
2024-09-24 13:59:07 -0500

    Renaming/consolidating existing unit tests

--- symbol_table/src/key/mod.rs
+++ symbol_table/src/key/mod.rs
@@ -59,7 +59,7 @@ impl From<NonNullableKey> for NullableKey {
 impl NonNullableKey {
     pub fn new<'a>(values: impl IntoIterator<Item = &'a Value> + Clone) -> Result<Self, Error> {
         if values.clone().into_iter().any(|x| x == &Value::empty()) {
-            Err(Error::SubKeyContainsNull)
+            Err(Error::SubKeyIsNull)
         } else {
             Ok(Self(NullableKey::new(values)?))
         }
@@ -178,46 +178,59 @@ mod tests {
     }
 
     #[test]
-    fn subscript_max_size() {
-        let result = NullableKey::new([&generate_value("a", MAX_SUB_LEN)]);
-        assert!(result.is_ok());
-    }
-    #[test]
-    fn subscript_that_is_to_large() {
-        let result = NullableKey::new([&generate_value("a", MAX_SUB_LEN + 1)]);
-        assert_eq!(result, Err(Error::SubscriptToLarge));
+    fn subscripts_have_a_max_size() {
+        assert!(NullableKey::new([&generate_value("a", MAX_SUB_LEN)]).is_ok());
+        assert_eq!(
+            NullableKey::new([&generate_value("a", MAX_SUB_LEN + 1)]),
+            Err(Error::SubscriptToLarge)
+        );
     }
 
     #[test]
-    fn key_max_size() {
-        let result = NullableKey::new([
+    fn keys_have_a_max_size() {
+        //End marker and internal type marker each take one byte.
+        const SUBSCRIPT_STORAGE_OVERHEAD: usize = 2;
+
+        //NOTE I have to use multiple subscripts due to limit on subscript length.
+        assert!(NullableKey::new([
             &generate_value("a", MAX_SUB_LEN),
-            //NOTE -4 to account for the 2 null terminators + 2 type markers
-            &generate_value("a", MAX_KEY_SIZE - MAX_SUB_LEN - 4),
-        ]);
-        assert!(result.is_ok());
+            &generate_value(
+                "a",
+                MAX_KEY_SIZE 
+                    - SUBSCRIPT_STORAGE_OVERHEAD //Overhead for storing this key.
+                    - (MAX_SUB_LEN + SUBSCRIPT_STORAGE_OVERHEAD) // Size of last key + overhead.
+            ),
+        ])
+        .is_ok());
+
+        assert_eq!(NullableKey::new([
+            &generate_value("a", MAX_SUB_LEN),
+            &generate_value(
+                "a",
+                MAX_KEY_SIZE 
+                - SUBSCRIPT_STORAGE_OVERHEAD //Overhead for storing this key.
+                - (MAX_SUB_LEN + SUBSCRIPT_STORAGE_OVERHEAD) // Size of last key + overhead.
+                +1 // Pushing us over the limit.
+            ),
+        ]),
+            Err(Error::KeyToLarge));
     }
 
-    #[test]
-    fn key_that_is_to_large() {
-        let result = NullableKey::new([
-            &generate_value("a", MAX_SUB_LEN),
-            &generate_value("a", MAX_SUB_LEN),
-        ]);
-        assert_eq!(result, Err(Error::KeyToLarge));
-    }
 
     #[test]
-    fn error_if_string_contains_null() {
+    fn subscript_values_canot_contain_null_byte() {
         let result = NullableKey::new([&"a\0b".try_into().unwrap()]);
         assert_eq!(result, Err(Error::SubKeyContainsNull));
     }
 
     #[test]
-    fn non_terminal_null() {
-        let key = NullableKey::new(&[Value::empty()]).expect("trailing null is fine");
-        let result = key.push(&"a".try_into().unwrap());
-        assert_eq!(result, Err(Error::SubKeyIsNull));
+    fn null_subscripts_can_only_be_the_last_subscript_of_a_nullable_key() {
+        let non_null_value = generate_value("a", 1);
+        assert!(NullableKey::new(&[Value::empty()]).is_ok());
+        assert!(NullableKey::new(&[non_null_value.clone(),Value::empty(),]).is_ok());
+        assert_eq!(NullableKey::new(&[Value::empty(),non_null_value]),Err(Error::SubKeyIsNull));
+
+        assert_eq!(NonNullableKey::new(&[Value::empty()]),Err(Error::SubKeyIsNull));
     }
 
     #[test]
@@ -269,7 +282,7 @@ mod tests {
     }
 
     #[test]
-    fn key_cmp() {
+    fn sorting_order_negative_positive_strings() {
         let keys: [NullableKey; 7] = ["", "-9.9", "-9", "0", "9", "9.9", "string"]
             .map(|x| NullableKey::new([&x.try_into().unwrap()]).unwrap());
         for [a, b] in keys.array_windows() {
