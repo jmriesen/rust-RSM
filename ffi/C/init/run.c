585dd9ddf23e4d6d00c243671b8b0266b32578f2
jmriesen <jacobriesen@gmail.com>
2024-10-13 11:19:18 -0500

    Removing reference to systab->maxjob

--- ffi/C/init/run.c
+++ ffi/C/init/run.c
@@ -117,7 +117,7 @@ NumberResult parse_env(char* env, uci_tab* uci_ptr ){
 
 jobtab* find_open_slot(jobtab *job_table, u_int table_size,u_char start_type,int pid){
     jobtab * job_tab = (jobtab *) NULL;                                            // clear jobtab pointer
-    for (u_int j = 0; j < systab->maxjob; j++) {                                // look for a free slot
+    for (u_int j = 0; j < table_size; j++) {                                // look for a free slot
         if (((job_table[j].pid == 0) && (start_type == TYPE_RUN)) ||     // this one ?
             ((job_table[j].pid == pid) && (start_type == TYPE_JOB))) {     // or already done (JOB)
             memset(&job_table[j], 0, sizeof(jobtab));                    // yes - zot the lot
