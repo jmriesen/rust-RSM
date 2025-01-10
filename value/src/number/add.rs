3cef10e191904f7468895d821fba5c3908861398
jmriesen <jacobriesen@gmail.com>
2025-01-09 23:53:00 -0600

    Pulling out normalizing logic

--- value/src/number/add.rs
+++ value/src/number/add.rs
@@ -7,7 +7,55 @@ impl ops::Add for Number {
     type Output = Number;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let mut lhs = self.0.0.clone();
+        let NormalizedDigits {
+            lhs,
+            rhs,
+            mut decimal_position,
+        } = Number::normalize_magnitieds(&self, &rhs);
+
+        let mut sum: Vec<_> = lhs
+            .iter()
+            .zip(rhs.iter())
+            .map(|(x, y)| x + y - b'0')
+            .collect();
+        // Handle carry over
+        for i in (1..sum.len()).rev() {
+            if sum[i] > b'9' {
+                sum[i] -= 10;
+                sum[i - 1] += 1;
+            }
+        }
+        // Handle carry over of most significant bit
+        if sum[0] > b'9' {
+            sum[0] -= 10;
+            sum.insert(0, b'1');
+            if let Some(dec_pos) = &mut decimal_position {
+                *dec_pos += 1;
+            }
+        }
+
+        Number::from_normalized(sum, decimal_position)
+    }
+}
+
+/// Stores digits in a normalized format.
+/// See `normalize_magnitieds`
+struct NormalizedDigits {
+    lhs: Vec<u8>,
+    rhs: Vec<u8>,
+    decimal_position: Option<usize>,
+}
+
+impl Number {
+    /// Takes two numbers and converses them to a vector of digit's.
+    ///
+    /// The resulting vectors have the same length and have there place values lined up.
+    /// For example 10 and .01 would result in
+    /// 1000
+    /// 0001
+    /// Some(2)
+    fn normalize_magnitieds(lhs: &Number, rhs: &Number) -> NormalizedDigits {
+        let mut lhs = lhs.0.0.clone();
         let mut rhs = rhs.0.0.clone();
         let lhs_int_len = lhs.iter().position(|x| *x == b'.').unwrap_or(lhs.len());
         let rhs_int_len = rhs.iter().position(|x| *x == b'.').unwrap_or(rhs.len());
@@ -49,43 +97,29 @@ impl ops::Add for Number {
             }
         }
 
-        // Temporarily remove decimal point (it makes the + logic simpler)
-        let mut dec_pos = lhs_dec_pos.or(rhs_dec_pos);
-        if let Some(dec_pos) = dec_pos {
+        // Pull out the decimal
+        let decimal_position = lhs_dec_pos.or(rhs_dec_pos);
+        if let Some(dec_pos) = decimal_position {
             rhs.remove(dec_pos);
             lhs.remove(dec_pos);
         }
-
-        let mut result: Vec<_> = lhs
-            .iter()
-            .zip(rhs.iter())
-            .map(|(x, y)| x + y - b'0')
-            .collect();
-        // Handle carry over
-        for i in (1..result.len()).rev() {
-            if result[i] > b'9' {
-                result[i] -= 10;
-                result[i - 1] += 1;
+        NormalizedDigits {
+            lhs,
+            rhs,
+            decimal_position,
+        }
+    }
+    fn from_normalized(mut digits: Vec<u8>, decimal_position: Option<usize>) -> Number {
+        if let Some(dec_pos) = decimal_position {
+            digits.insert(dec_pos, b'.');
+            while digits.last() == Some(&b'0') {
+                digits.pop();
+            }
+            if digits.last() == Some(&b'.') {
+                digits.pop();
             }
         }
-        if result[0] > b'9' {
-            result[0] -= 10;
-            result.insert(0, b'1');
-            if let Some(dec_pos) = &mut dec_pos {
-                *dec_pos += 1;
-            }
-        }
-
-        if let Some(dec_pos) = dec_pos {
-            result.insert(dec_pos, b'.');
-            while result.last() == Some(&b'0') {
-                result.pop();
-            }
-            if result.last() == Some(&b'.') {
-                result.pop();
-            }
-        }
-        Number(Value(result))
+        Number(Value(digits))
     }
 }
 
@@ -96,15 +130,15 @@ mod tests {
     use crate::Number;
 
     #[rstest]
-    #[case("1", "1", "2")]
-    #[case("10", "1", "11")]
-    #[case(".1", "1", "1.1")]
-    #[case("18", "7", "25")]
-    #[case("8", "7", "15")]
-    #[case("99.9", ".2", "100.1")]
-    #[case(".19", ".01", ".2")]
-    #[case(".9", ".1", "1")]
-    #[case("9", "1", "10")]
+    #[case::one_pluse_one("1", "1", "2")]
+    #[case::uint_of_different_length("10", "1", "11")]
+    #[case::decimial_and_int(".1", "1", "1.1")]
+    #[case::carry("18", "7", "25")]
+    #[case::carry_to_new_order_of_magnituid("8", "7", "15")]
+    #[case::carry_multiple_times("99.9", ".2", "100.1")]
+    #[case::remove_trailing_zero(".19", ".01", ".2")]
+    #[case::remove_trailing_dot(".9", ".1", "1")]
+    #[case::keep_int_traling_zero("9", "1", "10")]
     fn add(#[case] a: Number, #[case] b: Number, #[case] sum: Number) {
         assert_eq!(a.clone() + b.clone(), sum);
         assert_eq!(b + a, sum);
