e5a7807a708563af6d5304dec5846efe1860e177
jmriesen <jacobriesen@gmail.com>
2026-09-03 20:17:42 -0500

    Removing dead code that tracked indent/dedent.
    Code from an older attempt to do block parsing.

--- tree-sitter-M/grammar.js
+++ tree-sitter-M/grammar.js
@@ -7,9 +7,6 @@ var mumps_grammer = {
   word: $ => $.identifier,
   externals: $ => [
     $.VarUndefined,
-    $._indent,
-    $._dedent,
-    $._line_level,
     $.error_sentinel
   ],
 
@@ -24,10 +21,10 @@ var mumps_grammer = {
 
   rules: {
     source_file: $ => repeat1($.line),
-    line_level2: $ => ".",
+    line_level: $ => ".",
     line: $=> seq(
       optional(field('Tag',$.TagName)),
-      optional(seq(" ",repeat(field('level',$.line_level2)), field('commands',$.commands))),
+      optional(seq(" ",repeat(field('level',$.line_level)), field('commands',$.commands))),
       "\n"
     ),
     WriteArg: $ => choice(
@@ -71,7 +68,7 @@ var mumps_grammer = {
       optional(
         seq(
           "(",
-          //NOTE It is easier to just remove the traling VarUndefined when compiling then then durring parseing
+          //NOTE It is easier to just remove the trailing VarUndefined when compiling then then during parsing
           field("args",
             repeatDel(
               choice($.ByRef, $.Expression, $.VarUndefined),
@@ -83,7 +80,7 @@ var mumps_grammer = {
     ),
     ByRef: $ => seq(".", $.Variable),
 
-    //TODO identifiers should be constraind to 32 digets.
+    //TODO identifiers should be constrained to 32 digits.
     identifier: $ => /([A-Z]|[a-z])([A-Z]|[a-z]|\d)*/,
     sign: $ => repeat1(prec(1, choice("+", "-"))),
 
@@ -92,7 +89,7 @@ var mumps_grammer = {
       seq(".", /\d+/),
     ),
 
-    string: $ => /(\"([#-~]|[ ])*\")+/,  // all charactors from # to ~ excluding quote. //TODO check the actural specs latter.
+    string: $ => /(\"([#-~]|[ ])*\")+/,  // all characters from # to ~ excluding quote. //TODO check the actual specs latter.
 
     //-------------------------------
     //opcodes
@@ -333,7 +330,7 @@ Xcall = [
   ["E", false],
   ["Paschk", false],
   ["V", false],
-  //TODO using X again would cause name colition.
+  //TODO using X again would cause name coalition.
   //["X",false],
   ["Xrsm", false],
   ["SetEnv", true],
--- tree-sitter-M/src/scanner.c
+++ tree-sitter-M/src/scanner.c
@@ -34,26 +34,15 @@
 
 enum TokenType {
   VarUndefined,
-  INDENT,
-  DEDENT,
-  LINE_LEVEL,
   ERROR_SENTINEL
 };
 
 
 struct ParseStateStruct {
-  int indentation;
-  /*
-   * For now I want the entire document to be a block.
-   * this will be redone when I introduce proper tag handlaing.
-   */
-  bool docInitialized;
-} typedef ParseState;
+  } typedef ParseState;
 
 void *tree_sitter_mumps_external_scanner_create() {
   ParseState *state = malloc(sizeof(ParseState));
-  state->indentation = 0;
-  state->docInitialized= false;
   return state;
 }
 
@@ -77,52 +66,18 @@ void tree_sitter_mumps_external_scanner_deserialize(void *payload,
 
 bool tree_sitter_mumps_external_scanner_scan(void *payload, TSLexer *lexer,
                                              const bool *valid_symbols) {
-  // are we in error recoveray mode
-  if (valid_symbols[ERROR_SENTINEL]) {
-    return false;
-  }
-
-  // NOTE only valid as part of an args list.
-  // Therefore only valide if trailed by a ) or ,
-  if (valid_symbols[VarUndefined] &&
-      (lexer->lookahead == ',' || lexer->lookahead == ')')) {
-    lexer->result_symbol = VarUndefined;
-    return true;
-  } else if (valid_symbols[INDENT] || valid_symbols[DEDENT] ||
-             valid_symbols[LINE_LEVEL]) {
-
-    ParseState *state = payload;
-    lexer->mark_end(lexer);
-
-    // calculating indentation
-    //------------------------------------------------
-    int count = 0;
-
-    // Indentation must start with space or TODO tab.
-    if (lexer->lookahead == ' ') {
-      count++;
-      while (lexer->lookahead == '.' || lexer->lookahead == ' ') {
-        //After the initial indentation only '.' count
-        if (lexer->lookahead == '.') {
-          count++;
-        }
-        lexer->advance(lexer, false);
-      }
+    // are we in error recovery mode
+    if (valid_symbols[ERROR_SENTINEL]) {
+        return false;
     }
 
-    if (state->indentation < count && valid_symbols[INDENT]) {
-      lexer->result_symbol = INDENT;
-      state->indentation++;
-    } else if (state->indentation == count && valid_symbols[LINE_LEVEL]) {
-      lexer->result_symbol = LINE_LEVEL;
-      lexer->mark_end(lexer);
-    } else if (valid_symbols[DEDENT]) {
-      lexer->result_symbol = DEDENT;
-      state->indentation--;
-    } else {
-      return false;
+    // NOTE only valid as part of an args list.
+    // Therefore only validate if trailed by a ) or ,
+    if (valid_symbols[VarUndefined] &&
+        (lexer->lookahead == ',' || lexer->lookahead == ')')) {
+        lexer->result_symbol = VarUndefined;
+        return true;
+    }  else{
+        return false;
     }
-    return true;
-  }
-  return false;
 }
